89ec6e346587f9f66b9cf91a5c12d65c6d9201b0
Bakhomios Tadres <bakhomytadres@gmail.com>
2026-02-19 20:49:05 +0200

    Add Observer Feature with JS

--- script.js
+++ script.js
@@ -22,3 +22,65 @@ contactBtn.onclick = () => {
 // Year
 let year = document.getElementById("year");
 year.innerHTML = new Date().getFullYear();
+
+// Observer Intersection Animation for Service Cards
+const serviceCards = document.querySelectorAll("#services .card");
+const observerServicesCards = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((el) => {
+      el.target.classList.toggle("slide", el.isIntersecting);
+      if (el.isIntersecting) {
+        observerServicesCards.unobserve(el.target);
+      }
+    });
+  },
+  {
+    root: null,
+    rootMargin: "0px 0px 0px 400px",
+  },
+);
+serviceCards.forEach((card) => {
+  observerServicesCards.observe(card);
+});
+
+// Observer Intersection Animation for Projects Cards
+const projectCards = document.querySelectorAll(
+  ".projects .content .project-card",
+);
+const observerProjectCards = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((el) => {
+      el.target.classList.toggle("slide", el.isIntersecting);
+      if (el.isIntersecting) {
+        observerProjectCards.unobserve(el.target);
+      }
+    });
+  },
+  {
+    root: null,
+    rootMargin: "0px 0px 0px 400px",
+  },
+);
+projectCards.forEach((card) => {
+  observerProjectCards.observe(card);
+});
+
+// Observer Intersection Animation For Contacts Cards
+const contactCards = document.querySelectorAll(".contact .card");
+const observerContactCards = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((el) => {
+      el.target.classList.toggle("slide", el.isIntersecting);
+      if (el.isIntersecting) {
+        observerContactCards.unobserve(el.target);
+      }
+    });
+  },
+  {
+    root: null,
+    rootMargin: "0px 0px 0px 400px",
+  },
+);
+contactCards.forEach((card) => {
+  observerContactCards.observe(card);
+});
